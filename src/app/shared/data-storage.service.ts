bb627f98f5ce1c7b32b7a8a798f7614f69fe08c2
Colin Ronayne <cronayne@gmail.com>
2018-11-07 13:23:59 +0100

    new HttpClient

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { HttpClient, HttpParams, HttpRequest } from '@angular/common/http';
 import 'rxjs/Rx';
 
 import { RecipeService } from '../recipes/recipe.service';
@@ -7,25 +7,35 @@ import { Recipe } from '../recipes/recipe.model';
 import { AuthService } from '../auth/auth.service';
 
 @Injectable()
-export class DataStorageService{
-    constructor(private http: Http,
+export class DataStorageService {
+    constructor(private httpClient: HttpClient,
                 private recipeService: RecipeService,
                 private authService: AuthService) {}
 
     storeRecipes() {
         const token = this.authService.getToken();
 
-        return this.http.put('https://angular-http-71ef6.firebaseio.com/recipes.json?auth=' + token,
-                       this.recipeService.getRecipes());
+        // return this.httpClient.put('https://angular-http-71ef6.firebaseio.com/recipes.json',
+        //                this.recipeService.getRecipes(), {
+        //                    observe: 'body',
+        //                    params: new HttpParams().set('auth', token)
+        //                });
+
+        const req = new HttpRequest('PUT', 'https://angular-http-71ef6.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {
+            reportProgress: true, params: new HttpParams().set('auth', token)});
+        return this.httpClient.request(req);
     }
 
     getRecipes() {
         const token = this.authService.getToken();
 
-        this.http.get('https://angular-http-71ef6.firebaseio.com/recipes.json?auth=' + token)
+        // this.httpClient.get<Recipe[]>('https://angular-http-71ef6.firebaseio.com/recipes.json?auth=' + token)
+        this.httpClient.get<Recipe[]>('https://angular-http-71ef6.firebaseio.com/recipes.json?auth=' + token, {
+            observe: 'body',
+            responseType: 'json'
+        })
             .map(
-                (response: Response) => {
-                    const recipes: Recipe[] = response.json();
+                (recipes) => {
                     for (let recipe of recipes) {
                         if (!recipe['ingredients']) {
                             recipe['ingredients'] = [];
