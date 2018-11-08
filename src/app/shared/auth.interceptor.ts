357e65b5b8ccb860e80faa6adf2711d939d8346b
Colin Ronayne <cronayne@gmail.com>
2018-11-08 16:57:20 +0100

    Fixed Logout issue with .take(1)

--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -14,6 +14,7 @@ export class AuthInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log ('Intercepted!', req);
         return this.store.select('auth')
+            .take(1)
             .switchMap((authState: fromAuth.State) => {
                 const copiedReq = req.clone({params: req.params.set('auth', authState.token)});
                 return next.handle(copiedReq);
